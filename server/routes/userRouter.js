d0a5e488d9d948d9e06c27a8ee05ddc80a56348f
andresudi <y.andresudi@gmail.com>
2019-10-24 00:59:18 +0700

    test dr laptop abang

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -5,6 +5,6 @@ const authentication = require('../middlewares/authentication');
 router.post('/', UserController.create);
 router.post('/login', UserController.login);
 router.patch('/watchtag', authentication, UserController.updateTag);
-router.get('/', authentication, UserController.getDetail);
+router.get('/', UserController.getDetail);
 
 module.exports = router;
