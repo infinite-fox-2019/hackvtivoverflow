138c4be5d08409685f793e1bcb3bad96ec059087
andreassosilo <andreas.sosilo@gmail.com>
2019-10-24 14:21:45 +0700

    Modify routes in server

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,10 @@ const user = require('../routes/user')
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.status(200).json({ page: 'Home', message: 'Connected to Hacktivoverflow Apps!' })
+})
+
 router.use('/answer', answer)
 router.use('/question', question)
 router.use('/vote', vote)
