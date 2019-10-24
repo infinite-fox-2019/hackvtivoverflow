9cf489e7fa6c6c7e4cdf53a42f4b0eb54d56286d
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-10-24 18:08:15 +0700

    sudhartioeric

--- server/helpers/cronjob.js
+++ server/helpers/cronjob.js
@@ -2,27 +2,13 @@ const User = require('../models/user');
 const cron = require('node-cron');
 const sendMail = require('./sendMail')
 
-User.find()
-  .then(users => {
-    users.forEach((el, i) => {
-      cron.schedule("* * * 10 * *", function() {
-        console.log(`Send Schedule Mail`);
-        sendMail(
-          'ericsudhartio25@gmail.com',
-          'Thankyou for Trusting Hacktiv Overflow, please verify your account with reply this email "verify".. u can Register VIP member :)')
-      })
-    })
-  })
-  .catch(err => {
-    console.log(err)
-  })
 
 cron.schedule("* 5 5 5 * *", function() {
   User.find()
     .then(users => {
       users.forEach((el, i) => {
         console.log('Send Schedule Mail');
-        sendçMail(
+        sendMail(
           el.email,
           'Thankyou for Trusting Hacktiv Overflow, please verify your account with reply this email "verify".. u can Register VIP member :)')
       })
