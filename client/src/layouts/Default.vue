2dc4bd0a90358990833c517005b9467daca72486
akbfitra <akbarfitralistyono@gmail.com>
2019-10-24 18:42:26 +0700

    h8overflow

---
+++ client/src/layouts/Default.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="hHh Lpr fff">
+
+    <NavBar/>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar';
+export default {
+  name: 'DefaultLayout',
+  components: {
+    NavBar
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <q-drawer show-if-above v-model="left" :width="200" side="left" bordered>
+        <SideBar/>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+    
+    <q-page class="flex flex-center">
+      <img alt="Quasar logo" src="../assets/logo.png">
+    </q-page>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import SideBar from '../components/SideBar';
+export default {
+  name: 'PageHome',
+  components: {
+    SideBar
+    } 
+    
+}
+</script>
