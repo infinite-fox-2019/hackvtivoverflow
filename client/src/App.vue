25d6d57af15f7c4ba941e021782e3162f1f9680d
adityain98 <adityain98@gmail.com>
2019-10-25 11:48:54 +0700

    install cron

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,10 +8,35 @@
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import cron from 'cron'
 
 export default {
   components: {
     NavBar
+  },
+  created(){
+    const CronJob = cron.CronJob;
+    new CronJob('0 */5 * * * *', () => {
+      const token = localStorage.getItem('access_token')
+      if(token){
+        swal({
+          title: "Session End",
+          text: "Your session has end, want to log out?",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            this.$store.dispatch('logout')
+            this.$router.push('/login')
+            swal("Poof! Your imaginary file has been deleted!", {
+              icon: "success",
+            });
+          }
+        });
+      }
+    }, null, true, 'America/Los_Angeles');
   }
 }
 </script>
